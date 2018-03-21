23f240555cccdd9eac45524c27ed4ba6143cebe3
Peter Gray <peter@petergraycreative.com>
2018-03-21 15:13:35 -0500

    Add signup form, require auth action, and isAuthenticated functionality
    Pushes /signin to history if auth fails and goes to /users if auth is successful

--- src/components/HOC/RequireAuth.js
+++ src/components/HOC/RequireAuth.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { isAuthenticated } from '../../actions/index';
+import Users from '../users';
 
 export default ComposedComponent => {
   class RequireAuthentication extends Component {
     componentWillMount() {
+        this.props.isAuthenticated();
+        if (this.props.authenticated !== true) this.props.history.push('/signin');
       // Here, we want to check to see if `this.props.authenticated` is true
       // If it isn't, then redirect the user back to the /signin page
     }
 
     render() {
-      return <div />;
+      return (<div>
+        {this.props.authenticated === true
+            ? <ComposedComponent />
+            : <div>Not Logged In</div>
+          }
+      </div>);
       // Here, check to see if `this.props.authenticated` is true
       // If it isn't, then we don't want this component to return anything
       // Else, render the component that was passed to this higher-order component
@@ -22,5 +31,5 @@ export default ComposedComponent => {
     };
   };
 
-  return connect(mapStateToProps)(RequireAuthentication);
+  return connect(mapStateToProps, { isAuthenticated })(RequireAuthentication);
 };
